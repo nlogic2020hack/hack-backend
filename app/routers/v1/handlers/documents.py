5d3efc5e3e677b185ae2c516ba15bd755b98897d
MarkAntipin <antipinsuperstar@yandex.ru>
2020-11-01 01:12:38 +0300

    fix comments

--- app/routers/v1/handlers/documents.py
+++ app/routers/v1/handlers/documents.py
@@ -6,13 +6,22 @@ from settings.config import settings
 router = APIRouter()
 
 
+DOCTYPES = {
+    'technical_passport': 'Технический паспорт',
+    'contract': 'Договор аренды',
+    'operation_start_permission': 'Разрешение на ввод в эксплуатацию',
+    'construction_permission': 'Разрешение на строительство',
+    'AGR_certificate': 'Свидетельство AГР'
+}
+
+
 @router.get('/{id}')
 async def get_document(id: int):
     document = await Documents.get(id=id).prefetch_related('pages')
 
     result = {
         'id': document.id,
-        'type': document.type,
+        'type': DOCTYPES.get(document.type, document.type),
         'pages': [{
             'id': p.id,
             'url': f'{settings.BACKEND_URL}/v1/pages/{p.id}',
