d4d2a1ae6090588a7a7aca8b8986ce13a933023b
MarkAntipin <antipinsuperstar@yandex.ru>
2020-10-31 19:26:20 +0300

    fix page urls

--- app/routers/v1/handlers/pages.py
+++ app/routers/v1/handlers/pages.py
@@ -7,7 +7,7 @@ from app.database.models import Pages
 router = APIRouter()
 
 
-@router.get('/{id}')
-async def get_files(id: int):
+@router.get('/{id}', include_in_schema=False)
+async def get_pages(id: int):
     page_db = await Pages.get(id=id)
     return FileResponse(page_db.path, media_type='image/png')
